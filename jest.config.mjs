6ed8d63ce387b7cd4af6ba81d4e5ef57f75ace8c
Zay Lau <license@omniflex.io>
2024-12-07 15:04:14 +0800

    configured the jest moduleNameMapper

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,14 @@ export default {
   transform: {},
   preset: 'ts-jest',
   testEnvironment: 'node',
-  
+
+  moduleNameMapper: {
+    '@omniflex/core/?(.*)$': '<rootDir>/core/$1',
+    '@omniflex/infra-express/?(.*)$': '<rootDir>/infra/infra-express/$1',
+    '@omniflex/infra-postgres/?(.*)$': '<rootDir>/infra/infra-postgres/$1',
+    '@/(.*)$': '<rootDir>/apps/server/$1',
+  },
+
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '/dist/'
