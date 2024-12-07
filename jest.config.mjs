b7c5ba20b5460b3aa6e76dc5f704e7baad4308c9
Zay Lau <license@omniflex.io>
2024-12-07 15:34:30 +0800

    created the jest.setup.ts

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,12 +2,18 @@ export default {
   transform: {},
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  testMatch: [
+    '**/__tests__/**/*\.(spec|test)\.[jt]s?(x)',
+  ],
 
   moduleNameMapper: {
-    '@omniflex/core/?(.*)$': '<rootDir>/core/$1',
-    '@omniflex/infra-express/?(.*)$': '<rootDir>/infra/infra-express/$1',
-    '@omniflex/infra-postgres/?(.*)$': '<rootDir>/infra/infra-postgres/$1',
-    '@/(.*)$': '<rootDir>/apps/server/$1',
+    '^@omniflex/core$': '<rootDir>/core',
+    '^@omniflex/core/(.*)$': '<rootDir>/core/$1',
+    '^@omniflex/infra-express/?(.*)$': '<rootDir>/infra/infra-express/$1',
+    '^@omniflex/infra-postgres/?(.*)$': '<rootDir>/infra/infra-postgres/$1',
+    '^@/(.*)$': '<rootDir>/apps/server/$1',
   },
 
   coveragePathIgnorePatterns: [
