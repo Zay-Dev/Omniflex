70fda018b84ca295a0a22a2154ab079f3a0b952d
Zay Lau <license@omniflex.io>
2024-12-17 22:24:53 +0800

    removed the user-session module impl-express package

--- modules/module-user-session/impl-express/index.ts
+++
@@ -1,1 +0,0 @@
-export * as UserSessionValidators from './session.validation';
--- modules/module-user-session/impl-express/session.validation.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { tryValidateBody } from '@omniflex/infra-express/helpers/joi';
-import { schemas } from '@omniflex/module-user-session-core/session.schema';
-
-export const validateRefreshToken = [
-  tryValidateBody(schemas.refreshToken),
-];
