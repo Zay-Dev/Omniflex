be1fcc18b738753224e801826f54144deb1a3a2a
Zay Lau <license@omniflex.io>
2025-01-05 02:05:39 +0800

    revised the IRepository and migrated the implementations to it

--- infra/infra-mongoose/repositories.ts
+++ infra/infra-mongoose/repositories.ts
@@ -1,15 +1,16 @@
 import { BaseRepository } from './repositories/base';
 import { RawRepository } from './repositories/raw-repository';
-
-import {
-  TDeepPartial,
+import { 
+  TQueryFilter,
   TQueryOptions,
+  TQueryOperators,
   IBaseRepository,
-} from '@omniflex/core/types';
+} from '@omniflex/core/types/repository';
 
 export class MongooseBaseRepository<T, TPrimaryKey = string>
   extends BaseRepository<T>
   implements IBaseRepository<T, TPrimaryKey> {
+  
   raw() {
     return new RawRepository(this.model, {
       ...this.options,
@@ -20,133 +21,186 @@ export class MongooseBaseRepository<T, TPrimaryKey = string>
     return super.isValidDocumentId(id);
   }
 
-  async exists(filter: TDeepPartial<T>): Promise<boolean> {
-    const count = await this.model.countDocuments(
-      this.transformFilter(filter),
-      this.sharedQueryOptions,
-    );
-
+  async exists(filter: TQueryFilter<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<boolean> {
+    const count = await this.count(filter, options);
     return count > 0;
   }
 
-  findById(_id: TPrimaryKey, options?: TQueryOptions<T>): Promise<T | null> {
+  async count(filter: TQueryFilter<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<number> {
+    return this.model.countDocuments(
+      {
+        ...this.transformFilter(filter),
+        ...this.getParanoidFilter(options),
+      },
+      this.sharedQueryOptions,
+    );
+  }
+
+  findById(_id: TPrimaryKey, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
     return this.model.findOne(
       {
         _id,
         ...this.getParanoidFilter(options),
       },
       null,
-      this.transformQueryOptions(options)
+      this.sharedQueryOptions,
     );
   }
 
-  findOne(filter: TDeepPartial<T>, options?: TQueryOptions<T>): Promise<T | null> {
+  findOne(filter: TQueryFilter<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
     return this.model.findOne(
       {
         ...this.transformFilter(filter),
         ...this.getParanoidFilter(options),
       },
       null,
-      this.transformQueryOptions(options)
+      this.sharedQueryOptions,
     );
   }
 
-  find(filter: TDeepPartial<T>, options?: TQueryOptions<T>): Promise<T[]> {
+  find(filter: TQueryFilter<T>, options?: TQueryOptions<T>): Promise<T[]> {
     return this.model.find(
       {
         ...this.transformFilter(filter),
         ...this.getParanoidFilter(options),
       },
       null,
-      this.transformQueryOptions(options)
+      {
+        ...this.sharedQueryOptions,
+        skip: options?.skip,
+        limit: options?.take,
+        sort: options?.sort,
+      },
     );
   }
 
-  async count(filter: TDeepPartial<T>): Promise<number> {
-    return this.model.countDocuments(this.transformFilter(filter));
+  async create(data: Partial<T>): Promise<T> {
+    const doc = await this.model.create(data);
+    return doc.toObject();
   }
 
-  create(data: Partial<T>): Promise<T> {
-    const query = this.model.create(data);
-
-    return this.noAutoLean ? query :
-      query.then(result => result.toObject());
-  }
-
-  async updateById(_id: TPrimaryKey, data: Partial<T>): Promise<T | null> {
+  async updateById(_id: TPrimaryKey, data: Partial<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
     return this.model.findOneAndUpdate(
       {
         _id,
-        ...this.getParanoidFilter(),
+        ...this.getParanoidFilter(options),
       },
       data,
       {
         ...this.sharedQueryOptions,
         new: true,
-      }
+      },
     );
   }
 
-  async updateMany(filter: TDeepPartial<T>, data: Partial<T>) {
-    return (await this.model
-      .updateMany(
-        {
-          ...this.transformFilter(filter),
-          ...this.getParanoidFilter(),
-        },
-        data,
-        this.sharedQueryOptions,
-      )).modifiedCount;
+  async updateOne(filter: TQueryFilter<T>, data: Partial<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
+    return this.model.findOneAndUpdate(
+      {
+        ...this.transformFilter(filter),
+        ...this.getParanoidFilter(options),
+      },
+      data,
+      {
+        ...this.sharedQueryOptions,
+        new: true,
+      },
+    );
   }
 
-  async delete(_id: TPrimaryKey): Promise<boolean> {
-    const result = await this.model
-      .findOneAndDelete({ _id });
-
-    return !!result;
+  async update(filter: TQueryFilter<T>, data: Partial<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<number> {
+    const result = await this.model.updateMany(
+      {
+        ...this.transformFilter(filter),
+        ...this.getParanoidFilter(options),
+      },
+      data,
+      this.sharedQueryOptions,
+    );
+    return result.modifiedCount;
   }
 
-  async softDelete(_id: TPrimaryKey): Promise<boolean> {
-    const result = await this.model.findOneAndUpdate(
+  async deleteById(_id: TPrimaryKey): Promise<boolean> {
+    const result = await this.model.findOneAndDelete(
       {
         _id,
-        deletedAt: null,
       },
-      { deletedAt: new Date() },
-      { new: true }
+      this.sharedQueryOptions,
     );
-
     return !!result;
   }
 
-  protected transformQueryOptions<T>(options?: TQueryOptions<T>) {
-    if (!options) return this.sharedQueryOptions;
+  async deleteOne(filter: TQueryFilter<T>): Promise<boolean> {
+    const result = await this.model.findOneAndDelete(
+      {
+        ...this.transformFilter(filter),
+      },
+      this.sharedQueryOptions,
+    );
+    return !!result;
+  }
 
-    const transformed = {
-      ...this.sharedQueryOptions,
-      skip: options.skip,
-      limit: options.take,
-      sort: options.sort,
-    };
+  async delete(filter: TQueryFilter<T>): Promise<number> {
+    const result = await this.model.deleteMany(
+      {
+        ...this.transformFilter(filter),
+      },
+    );
+    return result.deletedCount;
+  }
 
-    if (options.select) {
-      transformed['select'] = Array.isArray(options.select) ?
-        options.select.join(' ') :
-        options.select;
+  async softDeleteById(_id: TPrimaryKey): Promise<boolean> {
+    const result = await this.updateById(_id, { deletedAt: new Date() } as any);
+    return !!result;
+  }
+
+  async softDeleteOne(filter: TQueryFilter<T>): Promise<boolean> {
+    const result = await this.updateOne(filter, { deletedAt: new Date() } as any);
+    return !!result;
+  }
+
+  async softDelete(filter: TQueryFilter<T>): Promise<number> {
+    return this.update(filter, { deletedAt: new Date() } as any);
+  }
+
+  protected transformFilter(filter: TQueryFilter<T>) {
+    if (!filter) return {};
+
+    const transformed = {};
+    for (const [key, value] of Object.entries(filter)) {
+      if (value === undefined) continue;
+
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        transformed[key] = this.transformOperators(value as TQueryOperators<any>);
+      } else {
+        transformed[key] = value;
+      }
     }
-
-    if (options.populate) {
-      transformed['populate'] = options.populate;
-    }
-
     return transformed;
   }
 
-  protected transformFilter(filter: TDeepPartial<T>) {
-    return filter;
+  protected transformOperators(operators: TQueryOperators<any>) {
+    if (!operators) return {};
+
+    const transformed = {};
+    for (const [key, value] of Object.entries(operators)) {
+      if (value === undefined) continue;
+
+      switch (key) {
+        case '$eq': transformed['$eq'] = value; break;
+        case '$ne': transformed['$ne'] = value; break;
+        case '$gt': transformed['$gt'] = value; break;
+        case '$gte': transformed['$gte'] = value; break;
+        case '$lt': transformed['$lt'] = value; break;
+        case '$lte': transformed['$lte'] = value; break;
+        case '$in': transformed['$in'] = value; break;
+        case '$nin': transformed['$nin'] = value; break;
+        case '$regex': transformed['$regex'] = value; break;
+      }
+    }
+    return transformed;
   }
 
-  protected getParanoidFilter(options?: TQueryOptions<T>) {
+  protected getParanoidFilter(options?: Pick<TQueryOptions<T>, 'paranoid'>) {
     if (options?.paranoid === false) return {};
     return { deletedAt: null };
   }
--- infra/infra-sequelize-v6/repositories.ts
+++ infra/infra-sequelize-v6/repositories.ts
@@ -1,13 +1,10 @@
 import { logger } from '@omniflex/core';
 import { Identifier, Op } from 'sequelize';
-
 import { BaseRepository } from './repositories/base';
 import { RawRepository } from './repositories/raw-repository';
-
 import {
-  TDeepPartial,
+  TQueryFilter,
   TQueryOptions,
-  TPopulateOption,
   TQueryOperators,
   IBaseRepository,
 } from '@omniflex/core/types/repository';
@@ -18,6 +15,7 @@ export class SequelizeRepository<
 >
   extends BaseRepository<T>
   implements IBaseRepository<T, TPrimaryKey> {
+
   raw() {
     return new RawRepository<T, TPrimaryKey>(this.model, {
       ...this.options,
@@ -43,236 +41,136 @@ export class SequelizeRepository<
     return true;
   }
 
-  async exists(filter: TDeepPartial<T>): Promise<boolean> {
-    const count = await this.model.count({
-      where: this.transformFilter(filter),
-    });
-
+  async exists(filter: TQueryFilter<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<boolean> {
+    const count = await this.count(filter, options);
     return count > 0;
   }
 
-  async findById(
-    id: TPrimaryKey,
-    options?: TQueryOptions<T>,
-  ): Promise<T | null> {
-    const result = await this.model.findByPk(
-      id,
-      this.transformQueryOptions(options),
-    );
-
-    return result?.toJSON() || null;
-  }
-
-  async findOne(
-    filter: TDeepPartial<T>,
-    options?: TQueryOptions<T>,
-  ): Promise<T | null> {
-    const result = await this.model.findOne({
-      ...this.transformQueryOptions(options),
-      where: this.transformFilter(filter)
-    });
-
-    return result?.toJSON() || null;
-  }
-
-  async find(
-    filter: TDeepPartial<T>,
-    options?: TQueryOptions<T>,
-  ): Promise<T[]> {
-    return (await this.model
-      .findAll({
-        ...this.transformQueryOptions(options),
-        where: this.transformFilter(filter),
-      }))
-      .map(entity => entity.toJSON());
-  }
-
-  async count(filter: TDeepPartial<T>): Promise<number> {
+  async count(filter: TQueryFilter<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<number> {
     return this.model.count({
       where: this.transformFilter(filter),
+      paranoid: options?.paranoid ?? true,
     });
+  }
+
+  async findById(id: TPrimaryKey, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
+    const result = await this.model.findByPk(id, {
+      paranoid: options?.paranoid ?? true,
+    });
+    return result?.toJSON() || null;
+  }
+
+  async findOne(filter: TQueryFilter<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
+    const result = await this.model.findOne({
+      where: this.transformFilter(filter),
+      paranoid: options?.paranoid ?? true,
+    });
+    return result?.toJSON() || null;
+  }
+
+  async find(filter: TQueryFilter<T>, options?: TQueryOptions<T>): Promise<T[]> {
+    const results = await this.model.findAll({
+      where: this.transformFilter(filter),
+      offset: options?.skip,
+      limit: options?.take,
+      order: options?.sort ?
+        Object.entries(options.sort).map(([key, value]) => [key, value.toUpperCase()]) :
+        undefined,
+      paranoid: options?.paranoid ?? true,
+    });
+    return results.map(result => result.toJSON());
   }
 
   async create(data: Partial<T>): Promise<T> {
-    const instance = await this.model.create(
-      data as any,
-      this.sharedQueryOptions,
-    );
-
-    return instance.toJSON();
+    const result = await this.model.create(data as any);
+    return result.toJSON();
   }
 
-  async updateById(id: TPrimaryKey, data: Partial<T>): Promise<T | null> {
-    await this.model.update(
-      data as any,
-      {
-        where: { id: id as any },
-        ...this.sharedQueryOptions,
-      }
-    );
-
-    return this.findById(id);
-  }
-
-  async updateMany(filter: TDeepPartial<T>, data: Partial<T>) {
-    return (await this.model.update(data, {
-      where: this.transformFilter(filter),
-      ...this.sharedQueryOptions,
-    }))[0];
-  }
-
-  async delete(id: TPrimaryKey) {
-    const result = await this.model.destroy({
+  async updateById(id: TPrimaryKey, data: Partial<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
+    const [count] = await this.model.update(data as any, {
       where: { id: id as any },
-      force: true
+      paranoid: options?.paranoid ?? true,
     });
-
-    return result > 0;
+    return count > 0 ? this.findById(id) : null;
   }
 
-  async softDelete(id: TPrimaryKey) {
-    const result = await this.model.destroy({
-      where: { id: id as any }
+  async updateOne(filter: TQueryFilter<T>, data: Partial<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<T | null> {
+    const [count] = await this.model.update(data as any, {
+      where: this.transformFilter(filter),
+      paranoid: options?.paranoid ?? true,
+      limit: 1,
     });
+    if (count === 0) return null;
 
-    return result > 0;
+    return this.findOne(filter);
   }
 
-  protected transformQueryOptions<T>(options?: TQueryOptions<T>) {
-    if (!options) return this.sharedQueryOptions;
-
-    const transformed = {
-      ...this.sharedQueryOptions,
-      offset: options.skip,
-      limit: options.take,
-      paranoid: options.paranoid ?? true,
-      order: options.sort ?
-        Object.entries(options.sort)
-          .map(([key, value]) =>
-            [key, (value as string).toUpperCase()]
-          ) :
-        undefined,
-    };
-
-    if (options.select) {
-      const fields = (Array.isArray(options.select) ?
-        options.select :
-        options.select.split(' '))
-        .map(String);
-
-      const included = fields
-        .filter(field => !field.startsWith('-'))
-        .map(field => field.startsWith('+') ? field.slice(1) : field);
-
-      const excluded = fields
-        .filter(field => field.startsWith('-'))
-        .map(field => field.slice(1));
-
-      if (included.length + excluded.length === 0) {
-        transformed['attributes'] = undefined;
-      } else {
-        const attributes = Object.keys(this.model.getAttributes())
-          .filter(attr => !excluded.includes(attr));
-
-        if (included.length > 0) {
-          transformed['attributes'] = attributes.filter(attr =>
-            included.includes(attr),
-          );
-        } else {
-          transformed['attributes'] = attributes;
-        }
-      }
-    }
-
-    if (options.populate) {
-      transformed['include'] = this.transformPopulateOption(options.populate);
-    }
-
-    return transformed;
+  async update(filter: TQueryFilter<T>, data: Partial<T>, options?: Pick<TQueryOptions<T>, 'paranoid'>): Promise<number> {
+    const [count] = await this.model.update(data as any, {
+      where: this.transformFilter(filter),
+      paranoid: options?.paranoid ?? true,
+    });
+    return count;
   }
 
-  private transformPopulateOption<T>(
-    populate: Array<keyof T | TPopulateOption<T>> | string | TPopulateOption<T>
-  ): any[] {
-    const handleSelect = (select: any) => this.transformSelect(
-      Array.isArray(select) ?
-        select.map(String) : String(select)
-    );
-
-    if (typeof populate === 'string') {
-      return populate.split(' ').map(field => ({
-        association: field
-      }));
-    }
-
-    if (Array.isArray(populate)) {
-      return populate.map(item => {
-        if (typeof item === 'object') {
-          const popOption = item as TPopulateOption<T>;
-
-          return {
-            association: popOption.path,
-            attributes: popOption.select ? handleSelect(popOption.select) : undefined,
-            include: popOption.populate ?
-              this.transformPopulateOption(popOption.populate) : undefined
-          };
-        }
-        return { association: item };
-      });
-    }
-
-    const popOption = populate as TPopulateOption<T>;
-    return [{
-      association: popOption.path,
-      attributes: popOption.select ? handleSelect(popOption.select) : undefined,
-      include: popOption.populate ?
-        this.transformPopulateOption(popOption.populate) : undefined
-    }];
+  async deleteById(id: TPrimaryKey): Promise<boolean> {
+    const count = await this.model.destroy({
+      where: { id: id as any },
+      force: true,
+    });
+    return count > 0;
   }
 
-  private transformSelect(select: string | string[]) {
-    if (typeof select === 'string') {
-      const fields = select.split(' ');
-      const included = fields
-        .filter(field => !field.startsWith('-'))
-        .map(field => field.startsWith('+') ? field.slice(1) : field);
-      const excluded = fields
-        .filter(field => field.startsWith('-'))
-        .map(field => field.slice(1));
-
-      if (included.length > 0) {
-        return included;
-      }
-
-      return { exclude: excluded };
-    }
-
-    return select;
+  async deleteOne(filter: TQueryFilter<T>): Promise<boolean> {
+    const count = await this.model.destroy({
+      where: this.transformFilter(filter),
+      force: true,
+      limit: 1,
+    });
+    return count > 0;
   }
 
-  protected transformFilter(filter: TDeepPartial<T>) {
+  async delete(filter: TQueryFilter<T>): Promise<number> {
+    return this.model.destroy({
+      where: this.transformFilter(filter),
+      force: true,
+    });
+  }
+
+  async softDeleteById(id: TPrimaryKey): Promise<boolean> {
+    const count = await this.model.destroy({
+      where: { id: id as any },
+    });
+    return count > 0;
+  }
+
+  async softDeleteOne(filter: TQueryFilter<T>): Promise<boolean> {
+    const count = await this.model.destroy({
+      where: this.transformFilter(filter),
+      limit: 1,
+    });
+    return count > 0;
+  }
+
+  async softDelete(filter: TQueryFilter<T>): Promise<number> {
+    return this.model.destroy({
+      where: this.transformFilter(filter),
+    });
+  }
+
+  protected transformFilter(filter: TQueryFilter<T>) {
     if (!filter) return {};
 
     const transformed = {};
-
     for (const [key, value] of Object.entries(filter)) {
-      if (value === undefined) {
-        transformed[key] = null;
-        continue;
-      }
+      if (value === undefined) continue;
 
-      if (value === null) {
-        transformed[key] = null;
-      } else if (typeof value === 'object' && !Array.isArray(value)) {
-        const transformedOps = this.transformOperators(value);
-        if (Object.keys(transformedOps).length > 0) {
-          transformed[key] = transformedOps;
-        }
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        transformed[key] = this.transformOperators(value as TQueryOperators<any>);
       } else {
         transformed[key] = value;
       }
     }
-
     return transformed;
   }
 
@@ -280,9 +178,8 @@ export class SequelizeRepository<
     if (!operators) return {};
 
     const transformed = {};
-
     for (const [key, value] of Object.entries(operators)) {
-      if (value === null || value === undefined) continue;
+      if (value === undefined) continue;
 
       switch (key) {
         case '$eq': transformed[Op.eq] = value; break;
@@ -296,7 +193,6 @@ export class SequelizeRepository<
         case '$regex': transformed[Op.regexp] = value; break;
       }
     }
-
     return transformed;
   }
 }
