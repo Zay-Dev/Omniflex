1ea94a4a78efb48e2fb4280618d4726250b7ed8e
Zay Lau <license@omniflex.io>
2025-03-17 22:43:22 +0800

    initialized the infras/databases/mongoose package

---
+++ infras/databases/mongoose/types.ts
@@ -0,0 +1,84 @@
+import { Schema } from 'mongoose';
+
+const toRequired = (type: Record<string, any>) => ({ ...type, required: true });
+
+export const optionalInteger = () => ({
+  type: Number,
+  get: Math.round,
+  set: Math.round,
+});
+export const requiredInteger = () => toRequired(optionalInteger());
+
+export const optionalNumber = () => ({ type: Number });
+export const requiredNumber = () => toRequired(optionalNumber());
+
+export const optionalString = () => ({ type: String });
+export const requiredString = () => toRequired(optionalString());
+export const toDefaultString = (defaultValue: string) => ({
+  ...optionalString(),
+  default: defaultValue,
+});
+
+export const toRequiredStringEnum = (values: string[]) => ({
+  ...requiredString(),
+  enum: values,
+});
+
+export const optionalDate = () => ({ type: Date });
+export const requiredDate = () => toRequired(optionalDate());
+
+export const optionalBoolean = () => ({ type: Boolean });
+export const requiredBoolean = () => toRequired(optionalBoolean());
+
+export const defaultFalse = () => ({ ...optionalBoolean(), default: false });
+export const defaultTrue = () => ({ ...optionalBoolean(), default: true });
+
+export const toOptionalObjectId = (ref: string) => ({
+  ref,
+  type: Schema.Types.ObjectId,
+});
+export const toRequiredObjectId = (ref: string) => {
+  return toRequired(toOptionalObjectId(ref));
+};
+
+export const toOptionalRefPathObjectId = (refPath: string) => ({
+  refPath,
+  type: Schema.Types.ObjectId,
+});
+export const toRequiredRefPathObjectId = (refPath: string) => {
+  return toRequired(toOptionalRefPathObjectId(refPath));
+};
+
+export const mixed = () => ({
+  type: Schema.Types.Mixed,
+  default: {},
+});
+export const toRequiredMixed = (defaultValue: Record<string, any> = {}) => ({
+  ...mixed(),
+  default: defaultValue,
+});
+
+export const subSchema = (
+  schema: Record<string, any>,
+  { _id }: { _id?: false; },
+) => {
+  return new Schema(schema, { _id });
+};
+
+export const deletedAt = () => ({ ...optionalDate(), default: null });
+
+export const preferredDate = () => ({
+  readable: requiredString(),
+  utcOffsetMinutes: requiredInteger(),
+
+  year: { ...requiredInteger(), min: 1 },
+  month: { ...requiredInteger(), min: 1 },
+  dayOfMonth: { ...requiredInteger(), min: 1 },
+});
+
+export const preferredTime = () => ({
+  ...preferredDate(),
+  hour: { ...requiredInteger(), min: 0 },
+  minute: { ...requiredInteger(), min: 0 },
+  totalMinutes: { ...requiredInteger(), min: 0 },
+});
